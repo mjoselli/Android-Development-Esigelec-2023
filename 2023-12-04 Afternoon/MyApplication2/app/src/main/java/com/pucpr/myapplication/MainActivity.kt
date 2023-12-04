322cd20b428ebf0d5eb4a954510131eafc1c2074
mjoselli <mejoselli@gmail.com>
2023-12-04 15:53:04 +0100

    04/12/2023 afternoon

---
+++ 2023-12-04 Afternoon/MyApplication2/app/src/main/java/com/pucpr/myapplication/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.pucpr.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    lateinit var editText:EditText
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        editText = findViewById<EditText>(R.id.editText)
+        editText.setText("0")
+    }
+    //one line comment
+    /*
+    multi
+    line
+    comment
+     */
+    public fun buttonClicked(view:View){
+        var number = editText.text.toString().toInt()
+        number += 3
+        editText.setText("$number")
+        var textView = findViewById<TextView>(R.id.textView)
+        textView.text = "Bye Esigelec: " + number
+        if(number > 10 && number < 25){
+            textView.text = "..."
+        }else if(number < 25){
+            textView.text = "Minor number "
+        }
+    }
+}
